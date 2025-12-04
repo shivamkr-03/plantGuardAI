8ba574e15ae7487781c54cdebcf5113ff02e5b81
shivamkr-03 <shivamkmr.0308@gmail.com>
2025-12-05 00:41:37 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -449,6 +449,5 @@ def predict():
 # Run
 # ---------------------------
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))
-    # debug=False for development to avoid interactive console
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
